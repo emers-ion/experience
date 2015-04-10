e25d65637525508e9ec085d22d6cc6cb4bc6849a
Jay <emers.ion@outlook.com>
2015-04-10 10:32:50 -0400

    Revert "main files"
    This reverts commit d24704293c28d1e7df23d4f981027aea227e47b7.

--- scripts/jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.circle').mouseenter(function() {
-    $('.circle').fadeTo('fast', 1);
-  });
-  $('.circle').mouseleave(function() {
-    $('.circle').fadeTo('fast', 0.5);
-  });
-});
