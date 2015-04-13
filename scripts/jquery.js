fcfef3d71fa26f96640d59564f55bcc6607cdb79
Jay <emers.ion@outlook.com>
2015-04-13 11:51:38 -0400

    added disapear when clicked need to fix reapperence when hoverstate is left

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
   $('.circle').mouseleave(function() {
     $('.circle').fadeTo('fast', 0.5);
   });
-  $('.circle').mouseenter(function() {
+  $('.circle').hover(function() {
     $(this).fadeTo('fast', 2);
+  });
+  $('.circle').click(function(){
+      $(this).fadeTo('fast', 0);
   });
 
 });
+
+
 
 // $(document).ready(function() {
 //   $('.bomb').click(function(){
